6e418fe207bd79954a9bdc17519d3b9d03a70264
Haley Jones <nyanidesuka@icloud.com>
2019-06-14 16:23:50 -0600

    Initial commit.

---
+++ GroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupProject
+//
+//  Created by Haley Jones on 6/14/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
