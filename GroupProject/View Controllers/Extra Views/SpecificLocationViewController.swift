225e93b490ed0f6ba0f4eb0daedad8523046e142
Dustin Koch <dustin.koch@me.com>
2019-06-17 12:38:08 -0600

    adds views and associated files

---
+++ GroupProject/View Controllers/Extra Views/SpecificLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpecificLocationViewController.swift
+//  GroupProject
+//
+//  Created by Dustin Koch on 6/17/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import UIKit
+
+class SpecificLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
