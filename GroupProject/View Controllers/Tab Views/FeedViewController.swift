9c182b326058151a5692e9f213de8875232774a7
Dustin Koch <dustin.koch@me.com>
2019-06-18 11:43:02 -0600

    adds outlets for feed

--- GroupProject/View Controllers/Tab Views/FeedViewController.swift
+++ GroupProject/View Controllers/Tab Views/FeedViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var locationImageView: UIImageView!
+    @IBOutlet weak var faqButton: UIButton!
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
