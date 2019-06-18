ccbae6b0e3e62ad0853e2d3648ce62c8adb62625
Haley Jones <nyanidesuka@icloud.com>
2019-06-18 15:40:00 -0600

    Adds JuiceNowReview and JuiceNowBusinessInfo models, and first tries a initializers to be used with Firestore.

---
+++ GroupProject/Models/JuiceNowBusinessReview.swift
@@ -0,0 +1,13 @@
+//
+//  JuiceNowReview.swift
+//  GroupProject
+//
+//  Created by Haley Jones on 6/18/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import Foundation
+
+class JuiceNowReview{
+    
+}
