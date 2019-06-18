198f97b836356d22e0b9e7f82fe84a52d2e6bd07
Dustin Koch <dustin.koch@me.com>
2019-06-18 13:55:01 -0600

    added outlets and notes for remaining views

--- GroupProject/View Controllers/Extra Views/LocationDetailsViewController.swift
+++ GroupProject/View Controllers/Extra Views/LocationDetailsViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class LocationDetailsViewController: UIViewController {
+    
+    //MARK: - Outlets
+    //Need outlet for location name
+    //Need outlet for rating score button
+    //Need outlet for review textview
+    //Need outlets for uploaded photos(s)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Actions
+    //Need action for tapping rating button
+    //Need action for uploading photo
+    
+    //MARK: - Helper Functions
+    func rateLocation() {
+        
+    }
+    func uploadPhoto() {
+        
     }
     
 
@@ -27,4 +45,4 @@ class LocationDetailsViewController: UIViewController {
     }
     */
 
-}
+}//END OF LOCATION DETAIL VIEW CONTROLLER
