1faf581c3bdb892399fec63a15e200e0cff2a1d4
Haley Jones <nyanidesuka@icloud.com>
2019-06-20 10:26:53 -0600

    Moves function to pull business details from yelp. Adds outline of function to create a JuiceNowBusinessInfo object from a yelp query.

--- GroupProject/Model Controllers/JuiceNowBusiessReviewController.swift
+++ GroupProject/Model Controllers/JuiceNowBusiessReviewController.swift
@@ -19,4 +19,6 @@ class JuiceNowBusinessReviewController{
         }
         return returnArray
     }
+    
+    
 }
--- GroupProject/Model Controllers/JuiceNowBusinessInfoController.swift
+++ GroupProject/Model Controllers/JuiceNowBusinessInfoController.swift
@@ -16,4 +16,43 @@ class JuiceNowBusinessInfoController{
         let infoDictionary: [String : Any] = ["reviews" : reviewsAsDictionaries, "businessID" : info.businessID]
         return infoDictionary
     }
+    
+    func fetchInfoForBusiness(business: Business, completion: @escaping ([String : Any]) -> Void){
+        let docName = business.businessID
+        FirebaseService.shared.fetchDocument(documentName: docName, collectionName: "Businesses") { (document) in
+            if let unwrappedDocument = document{
+                //if we're here, the document from firestore exists.
+                guard let businessDetails = JuiceNowBusinessInfo(dictionary: unwrappedDocument) else {return}
+                BusinessController.shared.addJuiceNowBusinessInfo(businessInfo: businessDetails, toBusiness: business)
+            } else {
+                //if we get here, the document did not exist in firestore
+                
+            }
+        }
+    }
+    
+    
+    //this function is gonna let us grab more details to put into the business. Hours, photo URLs
+    func fetchYelpDetails(forBusiness business: Business, completion: @escaping (JuiceNowBusinessInfo?) -> Void){
+        //build a URL
+        guard let baseURL = URL(string: "https://api.yelp.com/v3/businesses") else {completion(nil); return}
+        let finalURL = baseURL.appendingPathComponent(business.businessID)
+        YelpService.shared.fetch(url: finalURL) { (data) in
+            guard let unwrappedData = data else {completion(nil); return}
+            let detailDecoder = JSONDecoder()
+            do{
+                let detailTLD = try detailDecoder.decode(BusinessDetailTLD.self, from: unwrappedData)
+                print(detailTLD.photos.count)
+                print(detailTLD.photos[0])
+                business.hours = detailTLD.hours
+                business.imageURLs = detailTLD.photos
+                completion(nil)
+            }catch{
+                print("there was an error decoding the data.; \(error)")
+                print(unwrappedData)
+                completion(nil)
+                return
+            }
+        }
+    }
 }
