17c670c7792ec5464a55084ed9b57570eae48430
Dustin Koch <dustin.koch@me.com>
2019-06-20 11:02:19 -0600

    outlets and function signatures for location detail view controller

--- GroupProject/View Controllers/Extra Views/LocationDetailsViewController.swift
+++ GroupProject/View Controllers/Extra Views/LocationDetailsViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class LocationDetailsViewController: UIViewController {
     
     //MARK: - Outlets
-    //Need outlet for location name
-    //Need outlet for rating score button
-    //Need outlet for review textview
-    //Need outlets for uploaded photos(s)?
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var secondaryLocationNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var commuityStarOneButton: UIButton!
+    @IBOutlet weak var communityStarTwoButton: UIButton!
+    @IBOutlet weak var communityStarThreeButton: UIButton!
+    @IBOutlet weak var communityStarFourButton: UIButton!
+    @IBOutlet weak var communityStarFiveButton: UIButton!
+    @IBOutlet weak var personalStarOneButton: UIButton!
+    @IBOutlet weak var personalStarTwoButton: UIButton!
+    @IBOutlet weak var personalStarThreeButton: UIButton!
+    @IBOutlet weak var personalStarFourButton: UIButton!
+    @IBOutlet weak var personalStarFiveButton: UIButton!
+    
     
     //MARK: - Landing Pad / Properties
     var location: Business?
@@ -26,15 +36,45 @@ class LocationDetailsViewController: UIViewController {
     }
     
     //MARK: - Actions
-    //Need action for tapping rating button
-    //Need action for uploading photo
+    @IBAction func getDirectionsTapped(_ sender: UIButton) {
+    }
+    @IBAction func ratingOneTapped(_ sender: UIButton) {
+    }
+    @IBAction func ratingTwoTapped(_ sender: UIButton) {
+    }
+    @IBAction func ratingThreeTapped(_ sender: UIButton) {
+    }
+    @IBAction func ratingFourTapped(_ sender: UIButton) {
+    }
+    @IBAction func ratingFiveTapped(_ sender: UIButton) {
+    }
     
     //MARK: - Helper Functions
-    func rateLocation() {
-        
-    }
-    func uploadPhoto() {
-        
+    func ratingTappedFor(starNumber: Int) {
+        switch starNumber {
+        case 1:
+            //Change user rating for business to 1
+            //Update star images
+            print("One tapped")
+        case 2:
+            //Change user rating for business to 2
+            //Update star images
+            print("Two tapped")
+        case 3:
+            //Change user rating for business to 3
+            //Update star images
+            print("Three tapped")
+        case 4:
+            //Change user rating for business to 4
+            //Update star images
+            print("Four tapped")
+        case 5:
+            //Change user rating for business to 5
+            //Update star images
+            print("Five tapped")
+        default:
+            print("Error tapping rating❗️")
+        }
     }
     
 
