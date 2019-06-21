8dbf072acee92506e68a2d1640b05aa0367c5789
Jordan Hendrickson <jordanhendrickson@Jordans-MacBook-Pro.local>
2019-06-21 12:04:30 -0600

    adds style guide

---
+++ GroupProject/Custom UI/Style Guide.swift
@@ -0,0 +1,35 @@
+//
+//  Style Guide.swift
+//  GroupProject
+//
+//  Created by Jordan Hendrickson on 6/21/19.
+//  Copyright © 2019 DustinKoch. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addCornerRadius(_ radius: CGFloat = 4){
+        self.layer.cornerRadius = radius
+    }
+    func addAccentBorderColor(width: CGFloat = 1, color: UIColor = UIColor.lightBlue) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color.cgColor
+    }
+    func addLabelBorderColor(width: CGFloat = 1, color: UIColor = UIColor.greenAccent) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color.cgColor
+    }
+    func addViewBorderColor(width: CGFloat = 1, color: UIColor = UIColor.darkBlueAccent) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color.cgColor
+    }
+    
+}
+
+extension UIColor {
+    static let darkBlueAccent = UIColor(named: "DarkBlueAccent")!
+    static let greenAccent = UIColor(named: "GreenAccent")!
+    static let lightBlue = UIColor(named: "LightBlue")!
+    static let white = UIColor(named: "White")!
+}
