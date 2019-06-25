af0bcd1578e13e8796b160a1d0ce8a4e47c683b2
Haley Jones <nyanidesuka@icloud.com>
2019-06-25 15:38:54 -0600

    seeing if this fixes me

--- GroupProject/View Controllers/Extra Views/HelpViewController.swift
+++ GroupProject/View Controllers/Extra Views/HelpViewController.swift
@@ -32,7 +32,7 @@ class HelpViewController: UIViewController, MFMailComposeViewControllerDelegate 
 //        dismiss(animated: true, completion: nil)
     }
     
-<<<<<<< HEAD
+
     func configureMailController() -> MFMailComposeViewController{
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self
@@ -40,19 +40,5 @@ class HelpViewController: UIViewController, MFMailComposeViewControllerDelegate 
         mailComposerVC.setSubject("This is a testeroni")
         mailComposerVC.setMessageBody("This is a test message from JuiceNow, the app for REAL juicers.", isHTML: false)
         return mailComposerVC
-=======
-    @IBAction func selectErrorButtonTapped(_ sender: Any) {
-        
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
->>>>>>> aa96f6f376d710481bc020c689c8c4be04bc5936
     }
 }
