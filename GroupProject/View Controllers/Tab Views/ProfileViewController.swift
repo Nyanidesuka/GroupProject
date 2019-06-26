cde47c6ed1dbdfdf668df75bd01f883158764891
Dustin Koch <dustin.koch@me.com>
2019-06-26 10:01:04 -0600

    adds segue logic from profile

--- GroupProject/View Controllers/Tab Views/ProfileViewController.swift
+++ GroupProject/View Controllers/Tab Views/ProfileViewController.swift
@@ -41,34 +41,35 @@ class ProfileViewController: UIViewController {
             print("there's no image data")
             self.profilePhotoImageView.image = UIImage(named: "default")
         }
-        
     }
-    
-    //this is just here for testing
-    override func viewDidAppear(_ animated: Bool) {
-//        imagePicker.present(from: profilePhotoImageView)
-        //writing a manual segue to the  faq VC to be able to test it
-    }
+
+    //MARK: - Actions
     @IBAction func editProfileButtonTapped(_ sender: Any) {
         
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // IIDOO
+        switch segue.identifier {
+        case "toJuiceLocationDetail":
+            guard let indexPath = self.visitedCollectionView.indexPathsForSelectedItems?.first,
+            let destinationVC = segue.destination as? LocationDetailsViewController else { return }
+            let location = user?.likedBusinesses[indexPath.row]
+            destinationVC.location = location
+        case "toJuiceReviewDetail":
+            guard let indexPath = self.reviewTableView.indexPathForSelectedRow,
+                let destinationVC = segue.destination as? ReviewViewController else { return }
+            let review = user?.juiceReviews[indexPath.row]
+            destinationVC.review = review
+        default:
+            print("Error in segue from profile tab")
+        }
     }
-    */
-
 }//END OF PROFILE VIEW CONTROLLER
 
 extension ProfileViewController: ImagePickerDelegate {
     func didSelect(image: UIImage?) {
-        print(UserController.shared.currentUser?.username)
         guard let user = UserController.shared.currentUser else {print("couldnt unwrap the user🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️"); return}
         guard let image = image, let imageData = image.pngData() else {print("couldn't unwrap the image. 🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️"); return}
         user.photoData = imageData
@@ -84,7 +85,6 @@ extension ProfileViewController: ImagePickerDelegate {
 
 extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource{
    
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch collectionView{
         case visitedCollectionView:
@@ -93,24 +93,8 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
             return 0
         }
     }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        switch collectionView{
-        case visitedCollectionView:
-            return 1
-        default:
-            return 0
-        }
-    }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        if collectionView == reviewCollectionView{
-//            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reviewCell", for: indexPath) as? ReviewCollectionViewCell else {return UICollectionViewCell()}
-////            guard let review = user?.businessReviews[indexPath.row] else { return UICollectionViewCell() }
-//            cell.drinkNameLabel.text = "review.text"
-//            cell.restaurantNameLabel.text = "INSERT NAME OF LOCATION"
-//            return cell
-//        } else {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "juiceCell", for: indexPath) as? VisitedCollectionViewCell else {return UICollectionViewCell()}
             cell.juiceImageView.image = UIImage(named: "DefaultProfileImage")
             return cell
