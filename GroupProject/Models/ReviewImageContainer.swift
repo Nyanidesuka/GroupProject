b788213f6569927017fa35f9039f9823286160b2
Dustin Koch <dustin.koch@me.com>
2019-06-28 17:04:32 -0600

    fixes loading bug if new user

--- GroupProject/Models/ReviewImageContainer.swift
+++ GroupProject/Models/ReviewImageContainer.swift
@@ -13,6 +13,7 @@ class ReviewImageContainer{
     var images: [UIImage] = []
     
     func fetchAllReviewImages(forUser user: User, index: Int = 0, completion: @escaping (Bool) -> Void){
+        guard user.juiceReviews.count != 0 else { completion(true); return }
         FirebaseService.shared.fetchDocument(documentName: user.juiceReviews[index].imageDocReference, collectionName: "Images") { (imageDict) in
             guard let imageDict = imageDict, let imageData = imageDict["data"] as? Data, let decodedImage = UIImage(data: imageData) else {print("couldnt unwrap the image dictionary 👁‍🗨👁‍🗨👁‍🗨"); return}
             ReviewImageContainer.shared.images.append(decodedImage)
