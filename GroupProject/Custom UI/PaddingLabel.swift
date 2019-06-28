283aeca461b14796d082af351a47d45dde31c2aa
Dustin Koch <dustin.koch@me.com>
2019-06-28 10:13:01 -0600

    completes recipe detail page

--- GroupProject/Custom UI/PaddingLabel.swift
+++ GroupProject/Custom UI/PaddingLabel.swift
@@ -10,17 +10,21 @@ import Foundation
 import UIKit
 
 @IBDesignable class PaddingLabel: UILabel {
-    
+
     @IBInspectable var topInset: CGFloat = 8.0
     @IBInspectable var bottomInset: CGFloat = 8.0
     @IBInspectable var leftInset: CGFloat = 8.0
     @IBInspectable var rightInset: CGFloat = 8.0
     
+
     override func drawText(in rect: CGRect) {
         let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        
         super.drawText(in: rect.inset(by: insets))
+        
+        
     }
-    
+
     override var intrinsicContentSize: CGSize {
         let size = super.intrinsicContentSize
         return CGSize(width: size.width + leftInset + rightInset,
