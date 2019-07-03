7205fa80ac9f6a758a4274c5d71359ca3f607976
Dustin Koch <dustin.koch@me.com>
2019-07-03 14:03:50 -0600

    removes some comment code

--- GroupProject/Models/ReviewImageContainer.swift
+++ GroupProject/Models/ReviewImageContainer.swift
@@ -28,8 +28,6 @@ class ReviewImageContainer{
     }
     func fetchImagesForDetailPage(sender: LocationDetailsViewController, reviews: [JuiceReview], index:
         Int = 0, completion: @escaping (Bool) -> Void){
-        print("fetching review image for \(reviews[index].drinkName)⚙️⚙️⚙️")
-        print("fetching images for \(reviews.count) reviews.")
         guard reviews.count != 0 else { completion(true); return }
         FirebaseService.shared.fetchDocument(documentName: reviews[index].imageDocReference, collectionName: "Images") { (imageDict) in
             guard let imageDict = imageDict, let imageData = imageDict["data"] as? Data, let decodedImage = UIImage(data: imageData) else {print("couldnt unwrap the image dictionary 👁‍🗨👁‍🗨👁‍🗨"); return}
